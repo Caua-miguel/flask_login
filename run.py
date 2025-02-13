ea791d29eb05534d1bef79669b0ce645cf066cdf
Caua-miguel <caua.sudol@outlook.com>
2025-02-13 13:53:27 -0300

    test: login

--- run.py
+++ run.py
@@ -1,9 +1,28 @@
-from project import app
-from flask import render_template
+
+from flask import Flask, render_template, request, url_for, redirect
+from jinja2 import FileSystemLoader
+import os
+
+app = Flask(__name__)
+
+template_dirs = [os.path.join(os.path.dirname(__file__), 'project', 'users', 'templates'),
+                 os.path.join(os.path.dirname(__file__), 'project', 'templates')]
+
+app.jinja_loader = FileSystemLoader(template_dirs)
 
 @app.route('/')
 def home():
     return "Jumbas is a pachiderm!"
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid credentials! Please try again.'
+        else:
+            return redirect(url_for('home'))
+    return render_template('login.html', error=error)
 
 @app.route('/welcome')
 def welcome():
