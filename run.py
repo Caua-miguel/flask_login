1efb2c5eab8ae981c0e82bfceb8891094e5e204c
Caua-miguel <caua.sudol@outlook.com>
2025-02-17 14:05:26 -0300

    feat: database

--- run.py
+++ run.py
@@ -1,12 +1,14 @@
 
-from flask import Flask, render_template, request, url_for, redirect, session, flash
+from flask import Flask, render_template, request, url_for, redirect, session, flash, g
 from jinja2 import FileSystemLoader
 from functools import wraps
 import os
+import sqlite3
 
 app = Flask(__name__)
 
 app.secret_key = "123"
+# app.database = "database/storege/sample.db"
 
 # login required decorator
 def login_required(f):
@@ -27,7 +29,11 @@ app.jinja_loader = FileSystemLoader(template_dirs)
 @app.route('/')
 @login_required
 def home():
-    return render_template('index.html')
+    g.db = connect_db()
+    cur = g.db.execute('select * from posts')
+    posts = [dict(title=row[0], description=row[1]) for row in cur.fetchall()]
+    g.db.close()
+    return render_template('index.html', posts=posts)
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
@@ -47,6 +53,10 @@ def logout():
     session.pop('logged_in', None)
     flash('You were just logged out!')
     return redirect(url_for('welcome'))
+
+def connect_db():
+    db_path = os.path.join(os.path.dirname(__file__), 'database', 'storage', 'sample.db')
+    return sqlite3.connect(db_path)
 
 @app.route('/welcome')
 def welcome():
