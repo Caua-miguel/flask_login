6486d270d85cc905cb58d9a932d847fe063202b5
Caua-miguel <caua.sudol@outlook.com>
2025-03-02 14:27:48 -0300

    feat: created route users and add flask-cors

--- project/__init__.py
+++ project/__init__.py
@@ -1,17 +1,21 @@
 from flask import Flask
 import flask_login
+from flask_cors import CORS
 
 app = Flask(__name__)
 app.secret_key = '123'
+CORS(app)
 
 login_manager = flask_login.LoginManager()
 login_manager.init_app(app)
 
 from project.home.views import home_blueprint
 from project.auth.views import login_blueprint
+from project.users.views import user_blueprint
 
 app.register_blueprint(home_blueprint)
 app.register_blueprint(login_blueprint)
+app.register_blueprint(user_blueprint)
 
 # Importa o user_loader
 from project.auth.config import login
