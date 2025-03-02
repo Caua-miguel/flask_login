71d56e9b9419c2038575e44ebbd20ff9257ba168
Caua-miguel <caua.sudol@outlook.com>
2025-03-02 12:57:21 -0300

    refact: isolate SQL commands into .sql file

--- database/storage/sql.py
+++ database/storage/sql.py
@@ -1,8 +1,11 @@
 import sqlite3
 
+def ler_comandos_sql(path):
+    with open(path, 'r') as file:
+        return file.read()
+
 with sqlite3.connect("sample.db") as connection:
     c = connection.cursor()
     # c.execute("""DROP TABLE posts""")
-    c.execute("""CREATE TABLE posts(title TEXT, description TEXT)""")
-    c.execute('INSERT INTO posts VALUES("Good", "I\'m good.")')
-    c.execute('INSERT INTO posts VALUES("Well", "I\'m well.")')
+    comados_sql = ler_comandos_sql("sql.sql")
+    c.executescript(comados_sql)
---
+++ database/storage/sql.sql
@@ -0,0 +1,4 @@
+-- DROP TABLE posts;
+CREATE TABLE posts(title TEXT, description TEXT);
+INSERT INTO posts VALUES("Good", "I'm good.");
+INSERT INTO posts VALUES("Well", "I'm well.");
