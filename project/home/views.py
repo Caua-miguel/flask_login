6bf91b35b61f5f742e47e1f28c69da5ec9ea4b68
Caua-miguel <caua.sudol@outlook.com>
2025-03-08 19:53:05 -0300

    refact: update list to use new database

--- project/home/views.py
+++ project/home/views.py
@@ -1,29 +1,18 @@
-from flask import Blueprint, render_template, g
-import flask_login
-import sqlite3
-import os
+from flask import Blueprint, render_template
+from database.db import db
+from database.models import User_ORM
+from flask_login import login_required
 
 home_blueprint = Blueprint('home', __name__, template_folder='templates')
 
 @home_blueprint.route('/')
-@flask_login.login_required
+@login_required
 def home():
-    g.db = connect_db()
-    cur = g.db.execute('select * from posts')
-
-    posts = []
-
-    for row in cur.fetchall():
-        posts.append(dict(title=row[0], description=row[1]))
-        
-    g.db.close()
-    return render_template('index.html', posts=posts)
+    users = db.session.query(User_ORM).all()
+    print(users)
+    return render_template("index.html", users=users)
 
 @home_blueprint.route('/welcome')
 def welcome():
     return render_template('base.html')
-
-def connect_db():
-    db_path = os.path.join(os.path.dirname(__file__), '..', '..', 'database', 'sample.db')
-    return sqlite3.connect(db_path)
 
