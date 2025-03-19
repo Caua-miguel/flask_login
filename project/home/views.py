501fbb1f0e1fc991e6866e068748144d7f9c828e
Caua-miguel <caua.sudol@outlook.com>
2025-03-18 22:40:42 -0300

    refact: delete models from api

--- project/home/views.py
+++ project/home/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, jsonify
 from database.models import db
 from database.models import User_ORM
 from flask_login import login_required
@@ -9,9 +9,9 @@ home_blueprint = Blueprint('home', __name__, template_folder='templates')
 @login_required
 def home():
     users = db.session.query(User_ORM).all()
-    return render_template("index.html", users=users)
+    return jsonify({"Users: ": users})
 
 @home_blueprint.route('/welcome')
 def welcome():
-    return render_template('base.html')
+    return jsonify({"Message: ": "Welcome"})
 
