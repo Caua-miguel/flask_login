e4dfae617f746be955107c6c600ddcacdf8a9900
Caua-miguel <caua.sudol@outlook.com>
2025-03-19 23:47:22 -0300

    feat: add update user route

--- project/users/views.py
+++ project/users/views.py
@@ -26,6 +26,16 @@ def user():
     db.session.commit()
     return jsonify({'message': 'Usuário adicionado com sucesso!'}), 201
 
+@user_blueprint.route("/user/<string:id>/update", methods=["PUT"])
+def update_user(id):
+    
+    data = request.get_json() 
+    User_ORM.query.filter_by(id=id).update(data)
+    db.session.commit()
+
+    return jsonify({'message': 'Usuário atualizado com sucesso!'}), 201
+    
+
 @user_blueprint.route("/user/<string:id>/delete", methods=["GET", "DELETE"])
 def delete(id):
     user_by_id = db.get_or_404(User_ORM, id)
